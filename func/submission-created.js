a2eeba6f9463fa42947fd181b330912e8b3c16fe
rsfiye <30898632+rsfiye@users.noreply.github.com>
2021-06-23 09:26:03 -0500

    Update submission-created.js

--- func/submission-created.js
+++ func/submission-created.js
@@ -31,7 +31,7 @@ exports.handler = async function (event, context) {
         payload.token !== undefined) {
         
         const userInfo = decodeJwt(payload.token);
-        const BlockedUsers = ["188160596062109697"];
+        const BlockedUsers = [' '];
         if (BlockedUsers.indexOf(userInfo.id) > -1)
         {
             return {
